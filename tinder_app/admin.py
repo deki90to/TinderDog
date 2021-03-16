b544d4600d234f7a917eb14a8381556bf9b1a4dd
deki90to <deki90to@gmail.com>
2021-03-16 02:28:39 +0100

    changed look of admin page, enabled filtering/searching

--- tinder_app/admin.py
+++ tinder_app/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from . models import *
 
 
+
+
 admin.site.register(DogBreed)
-admin.site.register(Dog)
+# admin.site.register(Dog)
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+	list_display = ('user', 'breed', 'gender', 'birthYear', 'phone_no', 'date')
+	list_filter = ('date', 'breed', 'gender', 'birthYear', 'user')
+	ordering = ('-date',)
+	search_fields = ('gender', 'birthYear', 'info', 'phone_no', 'date')
